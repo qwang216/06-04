9b51f3cf109b60c1417df638e0241a69512d7de0
Jason Wang <qwang216@gmail.com>
2015-06-06 11:17:49 -0400

    second commit

--- twelvedaysofxmass/twelvedaysofxmass/main.m
+++ twelvedaysofxmass/twelvedaysofxmass/main.m
@@ -53,19 +53,4 @@ int main(int argc, const char * argv[]) {
  \n \n %@ %@ %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@
  \n \n %@ %@ %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@ \n %@"
  
- 
- On the first day of Christmas
- my true love sent to me:
- 12 Drummers Drumming
- 11 Pipers Piping
- 10 Lords a Leaping
- 9 Ladies Dancing
- 8 Maids a Milking
- 7 Swans a Swimming
- 6 Geese a Laying
- 5 Golden Rings
- 4 Calling Birds
- 3 French Hens
- 2 Turtle Doves
- and a Partridge in a Pear Tree
 */
